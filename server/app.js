db7cb52db8c7130ea97b24d765180289990f1be1
mukeshh24 <s.mukesh1403@gmail.com>
2026-06-19 11:41:25 +0530

    Initial frontend and backend setup

---
+++ server/app.js
@@ -0,0 +1,30 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+const PORT = process.env.PORT || 8000;
+
+// middlewares
+app.use(
+  cors({
+    origin: ["http://localhost:5173/"],
+    credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+  }),
+);
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// routes
+app.get("/", (req, res) => {
+  res.json({ status: 200, success: true, message: "Backend setup done!" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running in PORT : ${PORT}`);
+});
